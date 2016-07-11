7d34131f9ba428f9f5b7e6b1a5f94d6208889f0e
sdhoka <shubham.dhoka@reancloud.com>
2016-07-11 15:36:25 -0400

    adding changes

--- assignment1.rb
+++ assignment1.rb
@@ -3,18 +3,18 @@ puts 365 * 24
 
 # How many minutes in a decade
 #   8 years with 365 days and 2 years with 366 days
-puts (365 * 8.0 + 366 *2.0) * (24 * 60)
+puts (365 * 8.0 + 366 * 2.0) * (24 * 60)
 
 # How many seconds old are you
 # 22 years old
-puts 22* 365 * 24 * 3600
+puts 22 * 365 * 24 * 3600
 
 # Howmany chocolates do you hope to eat in lifetime
 #   Assuming that I live for 80 years and eat one chocolate every alternate day
 puts 40 * 1 * 365
 
 # If I am 1246 million years old, how old am I? 
-puts (1246.0 * 1000_000) / (3600 * 24 * 365)
+puts (1246.0 * 1_000_000) / (3600 * 24 * 365)
 
 
  
