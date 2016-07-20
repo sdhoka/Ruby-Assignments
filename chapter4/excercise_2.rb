398e76043abe064c3b9a96edd496cda37cc2ca30
sdhoka <shubham.dhoka@reancloud.com>
2016-07-20 14:02:59 -0400

    style changes

--- chapter4/excercise_2.rb
+++ chapter4/excercise_2.rb
@@ -3,8 +3,6 @@
 print "Enter your favorite no. "
 favorite_no = gets.chomp
 
-# Increment favorite_no 
-better_fav_no = favorite_no.to_i + 1
+favorite_no.to_i += 1
 
-# Display the better favorite no
-puts "The bigger and better favorite no. than #{favorite_no} is #{better_fav_no}"    
+puts "The bigger and better favorite no. is #{better_fav_no}"    
