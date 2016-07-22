3c5150b830cf9f90fd92922113bc43b12dc48e4b
sdhoka <shubham.dhoka@reancloud.com>
2016-07-22 12:18:48 -0400

    code for Chapter5

---
+++ chapter5/excercise_1.rb
@@ -0,0 +1,6 @@
+# Angry bird program
+
+puts "What do you want? "
+employee_need = gets.chomp
+
+print "Whaddaya mean \" #{employee_need} \"?!? You're fired!! \n"
---
+++ chapter5/excercise_2.rb
@@ -0,0 +1,8 @@
+
+# Visual formatting
+line_width = 40
+
+puts " Table of Contents ".center(line_width + 9)
+puts " #{"Chapter 1: Numbers".ljust(line_width)} page 1 "
+puts " #{"Chapter 2: Letters".ljust(line_width)} page 72 "
+puts " #{"Chapter 3: Variables".ljust(line_width)} page 118 "
