b7009c6205d88498f22a4f1bcc8023b749b99fd9
sdhoka <shubham.dhoka@reancloud.com>
2016-08-16 09:27:27 -0400

    added excercise_1a with use of yield
    style changes

--- chapter10/excercise_1a.rb
+++ chapter10/excercise_1a.rb
@@ -1,8 +1,8 @@
-# Grandfather Clock
-def clock(&block)
+# Program to test the use of yield
+def clock
   current_time = Time.now.hour > 12 ? Time.now.hour - 12 : Time.now.hour
 
-  current_time.times { block.call }
+  current_time.times { yield }
 end
 
 clock { puts 'DONG!' }
